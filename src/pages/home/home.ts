930c4356981f3c59d6b6342455b20dd21386619f
Caio Melzer de Oliveira <coliveira6@coliveira6-1.amer.globalcsc.net>
2018-01-12 20:19:41 -0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage{
 
-  constructor(public navCtrl: NavController) {
+  searchQuery: string = '';
+  btns: any;
 
+  constructor(private http: HttpClient) {
+    this.getAllPages();
+    
   }
 
+  getAllPages() {
+    this.http.get('http://www.porteirinha.com.br/wp-json/wp/v2/pages?per_page=100&status=publish&parent=0').subscribe(data => {
+      this.btns = data;
+      console.log(this.btns);
+    });
+  }
+
+  getPage(id){
+    console.log(id)
+  }
+
+  getBtns(ev: any) {
+    this.getAllPages();
+    let val = ev.target.value;
+    if (val && val.trim() != '') {
+      var btns =  this.btns.filter(function(item){
+        if(item.title.rendered.indexOf(val) > -1)
+          return item.title.rendered;
+      });
+      this.btns = btns;
+    }
+  }
 }
